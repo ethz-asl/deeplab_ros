4a1125b3cfa63363beea0b92ebd55c3241ab47de
Margarita Grinvald <margaritagrinvald@gmail.com>
2019-05-20 10:50:58 +0200

    Visualize as overlay

--- nodes/deeplab_ros_node.py
+++ nodes/deeplab_ros_node.py
@@ -7,6 +7,7 @@ from six.moves import urllib
 import PIL
 import numpy as np
 from skimage import transform
+import cv2
 
 import tensorflow as tf
 
@@ -19,9 +20,11 @@ from deeplab_ros import deeplab
 class DeepLabNode(object):
     def __init__(self):
         self._cv_bridge = CvBridge()
-
         self._last_msg = None
         self._msg_lock = threading.Lock()
+
+        self._publish_rate = rospy.get_param('~publish_rate', 100)
+        self._visualize = rospy.get_param('~visualize', True)
 
         rgb_input = rospy.get_param('~rgb_input', '/camera/rgb/image_color')
 
@@ -30,11 +33,6 @@ class DeepLabNode(object):
         self.label_pub = rospy.Publisher('~segmentation', Image, queue_size=1)
         self.vis_pub = rospy.Publisher('~segmentation_viz', Image, queue_size=1)
 
-        self._publish_rate = rospy.get_param('~publish_rate', 100)
-        self._visualize = rospy.get_param('~visualize', True)
-
-        # Valid model choices:
-        #'mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval'.
         MODEL_NAME = rospy.get_param('~model', 'mobilenetv2_coco_voctrainaug')
 
         _DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
@@ -53,13 +51,13 @@ class DeepLabNode(object):
         model_dir = tempfile.mkdtemp()
         tf.gfile.MakeDirs(model_dir)
         download_path = os.path.join(model_dir, _TARBALL_NAME)
-        rospy.logdebug('downloading model, this might take a while...')
+        print('Downloading model, this might take a while...')
         urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
                            download_path)
-        rospy.logdebug('download completed! loading DeepLab model...')
+        print('Download completed! Loading DeepLab model...')
 
-        self.model = deeplab.DeepLabModel(download_path)
-        rospy.logdebug('model loaded successfully!')
+        self._model = deeplab.DeepLabModel(download_path)
+        print('Model loaded successfully!')
 
     def run(self):
         rate = rospy.Rate(self._publish_rate)
@@ -76,7 +74,7 @@ class DeepLabNode(object):
             if msg is not None:
                 rgb_image = self._cv_bridge.imgmsg_to_cv2(msg, "passthrough")
 
-                # Run detection
+                # Run detection.
                 seg_map = self.detect(rgb_image)
 
                 rospy.logdebug("Publishing semantic labels.")
@@ -85,10 +83,9 @@ class DeepLabNode(object):
                 self.label_pub.publish(label_msg)
 
                 if self._visualize:
-                    # Visualize results
-                    seg_image = deeplab.label_to_color_image(seg_map.astype(
-                        np.int64)).astype(np.uint8)
-                    label_color_msg = self._cv_bridge.cv2_to_imgmsg(seg_image, 'rgb8')
+                    # Overlay segmentation on RGB image.
+                    image = self.visualize(rgb_image, seg_map)
+                    label_color_msg = self._cv_bridge.cv2_to_imgmsg(image, 'bgr8')
                     label_color_msg.header = msg.header
                     self.vis_pub.publish(label_color_msg)
 
@@ -98,13 +95,21 @@ class DeepLabNode(object):
     def detect(self, rgb_image):
         rgb_image = PIL.Image.fromarray(rgb_image)
 
-        resized_im, seg_map = self.model.run(rgb_image)
+        resized_im, seg_map = self._model.run(rgb_image)
         target_size = rgb_image.size[::-1]
         seg_map = transform.resize(
             seg_map, target_size, order=0, preserve_range=True).astype(
                 np.uint16)
 
         return seg_map
+
+    def visualize(self, rgb_image, seg_map, alpha = 0.6):
+        image = rgb_image.copy()
+        seg_image = deeplab.label_to_color_image(seg_map.astype(
+            np.int64)).astype(np.uint8)
+        cv2.addWeighted(seg_image, alpha, image, 1 - alpha, 0, image)
+        return image
+
 
     def _image_callback(self, msg):
         rospy.logdebug("Got an image.")
